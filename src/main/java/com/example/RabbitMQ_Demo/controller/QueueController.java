3dab904232a603b71879cd4724c29d288914647c
jgzhernandez <jettlaagg@gmail.com>
2024-12-11 10:25:14 +0800

    Initial commit

---
+++ src/main/java/com/example/RabbitMQ_Demo/controller/QueueController.java
@@ -0,0 +1,20 @@
+package com.example.RabbitMQ_Demo.controller;
+
+import com.example.RabbitMQ_Demo.QueueSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testq")
+public class QueueController {
+    @Autowired
+    private QueueSender queueSender;
+
+    @GetMapping
+    public String send(){
+        queueSender.send("test message");
+        return "ok. done";
+    }
+}
